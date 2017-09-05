79fec2e19a0eae8531489e443e7a922fe9a2d3ca
Troy Molnar <troy.molnar25@gmail.com>
2017-09-05 19:20:04 -0400

    seperating out speech code vs UI code

--- app/src/main/java/com/adc/td/assistant/MainActivity.java
+++ app/src/main/java/com/adc/td/assistant/MainActivity.java
@@ -1,57 +1,36 @@
 package com.adc.td.assistant;
 
 import android.Manifest;
-import android.content.ComponentName;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.IBinder;
-import android.speech.tts.TextToSpeech;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.firebaseDementia.R;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import ai.api.AIListener;
-import ai.api.AIServiceException;
-import ai.api.android.AIConfiguration;
-import ai.api.android.AIService;
 import ai.api.model.AIError;
-import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
 
-import static android.speech.tts.TextToSpeech.QUEUE_FLUSH;
-
-public class MainActivity extends AppCompatActivity implements AIListener, TextToSpeech.OnInitListener, MessageDialogFragment.Listener {
+public class MainActivity extends AppCompatActivity implements MessageDialogFragment.Listener {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static final String CLIENT_API_TOKEN = "4bc68760f7b34a458d4acaf4a4552e10";
 
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
-
     private static final String FRAGMENT_MESSAGE_DIALOG = "message_dialog";
 
     TextView instructionView;
     EditText queryText;
     TextView resultView;
-    TextToSpeech tts;
-    AIService aiService;
 
-    private SpeechService speechService;
-    private VoiceRecorder voiceRecorder;
+    SpeechWrapper speechWrapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,197 +41,105 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
         queryText = findViewById(R.id.editText);
         Button micOnButton = findViewById(R.id.mic_on);
         Button micOffButton = findViewById(R.id.mic_off);
-        Button okButton = findViewById(R.id.button);
+        Button submitButton = findViewById(R.id.button);
         resultView = findViewById(R.id.result);
 
-        tts = new TextToSpeech(this, this);
-
-        final AIConfiguration config = new AIConfiguration(CLIENT_API_TOKEN,
-                AIConfiguration.SupportedLanguages.English,
-                AIConfiguration.RecognitionEngine.System);
-
-        aiService = AIService.getService(this, config);
-
-        aiService.setListener(this);
-
+        speechWrapper = new SpeechWrapper(this);
+        speechWrapper.addCallback(callback);
 
         micOnButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startGoogleCloudVoice();
+                openVoiceRecording();
             }
         });
 
         micOffButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                stopVoiceRecorder();
-                sendAiRequest(queryText.getText().toString());
+                speechWrapper.stopListening();
+                String partialQuery = queryText.getText().toString();
+                if (!TextUtils.isEmpty(partialQuery)) {
+                    speechWrapper.sendAiRequest(partialQuery);
+                }
             }
         });
 
-        okButton.setOnClickListener(new View.OnClickListener() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendAiRequest(queryText.getText().toString());
+                speechWrapper.sendAiRequest(queryText.getText().toString());
             }
         });
     }
 
-
-    ////////////////////////////////////AILISTENER////////////////////////////////////
-    @Override
-    public void onResult(AIResponse aiResponse) {
-
-
-        StringBuilder builder = new StringBuilder();
-        Result result = aiResponse.getResult();
-        String speech = result.getFulfillment().getSpeech();
-        String resolvedQuery = result.getResolvedQuery();
-        builder.append("QUERY: ").append(resolvedQuery).append("\n\n")
-                .append("ACTION: ").append(result.getAction()).append("\n\n")
-                .append("STATUS: ").append(aiResponse.getStatus()).append("\n\n")
-                .append("RESPONSE: ").append(speech).append("\n\n").append("RAW: ")
-                .append(aiResponse);
-        queryText.setText(resolvedQuery);
-        resultView.setText(builder.toString());
-        tts.speak(speech, QUEUE_FLUSH, null, "UTTERANCEID"+System.currentTimeMillis());
-
-        try {
-            // Write a message to the database
-            FirebaseDatabase database = FirebaseDatabase.getInstance();
-            DatabaseReference myRef = database.getReference("Dementia Logs");
-
-            myRef.push().setValue(resolvedQuery + " : " + speech );
-        }catch(Exception e) {
-            Log.e("crash", "Error", e);
-        }
-
-        Log.d("firebase logs", "Save " + resolvedQuery + " " + speech + " " + result.getAction());
-    }
-
-    @Override
-    public void onError(AIError error) {
-        instructionView.setText(R.string.default_instruction);
-        resultView.setText(String.format("ERROR: %s", error.getMessage()));
-        Log.e(TAG, "AILISTENER onError: " + error.getMessage());
-    }
-
-    @Override
-    public void onAudioLevel(float level) {
-
-    }
-
-    @Override
-    public void onListeningStarted() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                instructionView.setText(R.string.listening);
-                queryText.setText(null);
-            }
-        });
-    }
-
-    @Override
-    public void onListeningCanceled() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                instructionView.setText(R.string.default_instruction);
-            }
-        });
-    }
-
-    @Override
-    public void onListeningFinished() {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                instructionView.setText(R.string.listening_finished);
-            }
-        });
-    }
-
-    @Override
-    public void onInit(int i) {
-        Toast.makeText(this, "tts has initialized", Toast.LENGTH_SHORT).show();
-    }
-
-
-    private void sendAiRequest(String query) {
-        AIRequest request;
-        try {
-            request = new AIRequest(query);
-        } catch (Exception e) {
-            Log.e(TAG, "failed to create AiRequest", e);
-            resultView.setText(e.getMessage());
-            return;
-        }
-        new AsyncTask<AIRequest, Void, AIResponse>() {
-            private Exception exception = null;
-            @Override
-            protected AIResponse doInBackground(AIRequest... requests) {
-                final AIRequest request = requests[0];
-                try {
-                    return aiService.textRequest(request);
-                } catch (AIServiceException e) {
-                    exception = e;
-                    Log.e(TAG, "error sending text request", e);
+    private SpeechWrapper.Callback callback = new SpeechWrapper.Callback() {
+        @Override
+        public void onListeningStarted() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    instructionView.setText(R.string.listening);
+                    queryText.setText(null);
                 }
-                return null;
-            }
+            });
+        }
 
-            @Override
-            protected void onPostExecute(AIResponse aiResponse) {
-                if (aiResponse != null) {
-                    // process aiResponse here
-                    onResult(aiResponse);
-                } else if (exception != null) {
-                    resultView.setText(String.format("TEXT ERROR: %s", exception.getMessage()));
+        @Override
+        public void onListeningPartial(@NonNull final String partialQuery) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    queryText.setText(partialQuery);
                 }
-            }
-        }.execute(request);
-    }
-
-    private final VoiceRecorder.Callback voiceCallback = new VoiceRecorder.Callback() {
-
-        @Override
-        public void onVoiceStart() {
-            Log.d(TAG, "onVoiceStart");
-            onListeningStarted();
-            if (speechService != null) {
-                speechService.startRecognizing(voiceRecorder.getSampleRate());
-            }
+            });
         }
 
         @Override
-        public void onVoice(byte[] data, int size) {
-//            Log.d(TAG, "onVoice");
-            if (speechService != null) {
-                speechService.recognize(data, size);
-            }
+        public void onListeningFinished() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    instructionView.setText(R.string.listening_finished);
+                }
+            });
         }
 
         @Override
-        public void onVoiceEnd() {
-            Log.d(TAG, "onVoiceEnd");
-            onListeningFinished();
-            if (speechService != null) {
-                speechService.finishRecognizing();
-            }
+        public void onComplete(@NonNull AIResponse aiResponse) {
+            StringBuilder builder = new StringBuilder();
+            Result result = aiResponse.getResult();
+            String speech = result.getFulfillment().getSpeech();
+            String resolvedQuery = result.getResolvedQuery();
+            builder.append("QUERY: ").append(resolvedQuery).append("\n\n")
+                    .append("ACTION: ").append(result.getAction()).append("\n\n")
+                    .append("STATUS: ").append(aiResponse.getStatus()).append("\n\n")
+                    .append("RESPONSE: ").append(speech).append("\n\n").append("RAW: ")
+                    .append(aiResponse);
+            queryText.setText(resolvedQuery);
+            resultView.setText(builder.toString());
         }
 
+        @Override
+        public void onError(@NonNull AIError error) {
+            instructionView.setText(R.string.default_instruction);
+            resultView.setText(String.format("ERROR: %s", error.getMessage()));
+        }
+
+        @Override
+        public void onException(@NonNull Exception e) {
+            instructionView.setText(R.string.default_instruction);
+            resultView.setText(String.format("ERROR: %s", e.getMessage()));
+        }
     };
 
-    private void startGoogleCloudVoice() {
-        // Prepare Cloud Speech API
-        bindService(new Intent(this, SpeechService.class), serviceConnection, BIND_AUTO_CREATE);
+    private void openVoiceRecording() {
+        speechWrapper.bindService();
 
         // Start listening to voices
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
                 == PackageManager.PERMISSION_GRANTED) {
-            startVoiceRecorder();
+            speechWrapper.startListening();
         } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                 Manifest.permission.RECORD_AUDIO)) {
             showPermissionMessageDialog();
@@ -262,73 +149,15 @@ public class MainActivity extends AppCompatActivity implements AIListener, TextT
         }
     }
 
-    private void startVoiceRecorder() {
-        Log.d(TAG, "voice recorder started");
-        if (voiceRecorder != null) {
-            voiceRecorder.stop();
-        }
-        voiceRecorder = new VoiceRecorder(voiceCallback);
-        voiceRecorder.start();
-    }
-
-    private void stopVoiceRecorder() {
-        if (voiceRecorder != null) {
-            voiceRecorder.stop();
-            voiceRecorder = null;
-        }
-        instructionView.setText(R.string.listening_finished);
-    }
-
     private void showPermissionMessageDialog() {
         MessageDialogFragment
                 .newInstance(getString(R.string.permission_message))
                 .show(getSupportFragmentManager(), FRAGMENT_MESSAGE_DIALOG);
     }
 
-    private final ServiceConnection serviceConnection = new ServiceConnection() {
-
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder binder) {
-            Log.d(TAG, "onServiceConnected");
-            speechService = SpeechService.from(binder);
-            speechService.addListener(speechServiceListener);
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            Log.d(TAG, "onServiceDisconnected");
-            speechService = null;
-        }
-
-    };
-
     @Override
     public void onMessageDialogDismissed() {
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
                 REQUEST_RECORD_AUDIO_PERMISSION);
     }
-
-    private final SpeechService.Listener speechServiceListener =
-            new SpeechService.Listener() {
-                @Override
-                public void onSpeechRecognized(final String text, final boolean isFinal) {
-                    if (isFinal) {
-                        voiceRecorder.dismiss();
-                    }
-                    if (text != null && !TextUtils.isEmpty(text)) {
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                if (isFinal) {
-                                    stopVoiceRecorder();
-                                    resultView.setText(null);
-                                    sendAiRequest(text);
-                                } else {
-                                    resultView.setText(text);
-                                }
-                            }
-                        });
-                    }
-                }
-            };
 }
